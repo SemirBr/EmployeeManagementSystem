fe32e236e5b1c1841a44d4da432d5a7e2d202b96
Semir Brahimbashev <brahimbashev@abv.bg>
2023-12-18 03:06:03 +0200

    Create Project.java
    Create a second Entity Project

---
+++ EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/entity/Project.java
@@ -0,0 +1,28 @@
+package com.example.EmployeeManagementSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private long employeeId;
+    @Column(name="name_of_project")
+    private String nameOfProject;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date dateFrom;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date dateTo;
+}

97e54eceee50fb5e8f3667536b8c9cc351455106
Semir Brahimbashev <brahimbashev@abv.bg>
2023-12-21 01:18:22 +0200

    Create EmployeeProjectSystemController and ProjectController
    Create EmployeeProjectSystemController and ProjectController with their CRUD operations

---
+++ EmployeeManagementSystem/src/main/java/com/example/EmployeeManagementSystem/controller/ProjectController.java
@@ -0,0 +1,49 @@
+package com.example.EmployeeManagementSystem.controller;
+
+import com.example.EmployeeManagementSystem.dto.ProjectDto;
+import com.example.EmployeeManagementSystem.service.ProjectService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<ProjectDto> createProject(@RequestBody ProjectDto projectDto) {
+        ProjectDto savedProject = projectService.createProject(projectDto);
+        return new ResponseEntity<>(savedProject, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ProjectDto> getProjectById(@PathVariable("id") Long projectId) {
+        ProjectDto projectDto = projectService.getProjectById(projectId);
+        return ResponseEntity.ok(projectDto);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProjectDto>> getAllProjects() {
+        List<ProjectDto> projects = projectService.getAllProjects();
+        return ResponseEntity.ok(projects);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<ProjectDto> updateProject(@PathVariable("id") Long projectId,
+                                                    @RequestBody ProjectDto updatedProject) {
+        ProjectDto projectDto = projectService.updateProject(projectId, updatedProject);
+        return ResponseEntity.ok(projectDto);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteProject(@PathVariable("id") Long projectId) {
+        projectService.deleteProject(projectId);
+        return ResponseEntity.ok("Project deleted successfully");
+    }
+}
